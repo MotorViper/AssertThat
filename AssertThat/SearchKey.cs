4be7dbb1f25759b7de7e2c66911def82f6813415
Mark Jeffries <mark.c.jeffries@gmail.com>
2025-10-02 17:35:44 +0100

    Updated SearchKey

--- AssertThat/SearchKey.cs
+++ AssertThat/SearchKey.cs
@@ -2,29 +2,55 @@ using System.Text.RegularExpressions;
 
 namespace AssertThatLibrary;
 
-public class SearchKey
+/// <summary>
+///     Class to allow keyword matching using a given method,
+///     which can be any of straight text, regular expression, wildcard (with or without ?) or numeric wildcard.
+/// </summary>
+public class SearchKey : IEquatable<string>
 {
     private readonly string _key;
 
+    /// <summary>
+    ///     Creates a search key for straight text matching.
+    /// </summary>
+    /// <param name="key">The key which cannot be null.</param>
     public SearchKey(string key)
     {
-        _key = key;
+        _key = key ?? throw new SearchKeyException("Search key must not be null");
         Regex = null;
     }
 
+    /// <summary>
+    ///     Creates a search key with a regular expression.
+    /// </summary>
+    /// <param name="key">The regular expression which cannot be null.</param>
     public SearchKey(Regex key)
     {
         _key = "";
-        Regex = key;
+        Regex = key ?? throw new SearchKeyException("Search key must not be null");
     }
 
+    /// <summary>
+    ///     The equivalent regular expression, if relevant.
+    /// </summary>
     public Regex? Regex { get; }
 
+    /// <inheritdoc />
+    public bool Equals(string? other) => other != null && (Regex?.IsMatch(other) ?? _key == other);
+
+    /// <summary>
+    ///     Create a search key. The key can have any of the following formats:
+    ///     "xxx" - straight text match
+    ///     "D:xxx" - a numeric wildcard match, '*' = any character, '?' = single character, '#' - numeric character
+    ///     "R:xxx" - a regular expression match
+    ///     "S:xxx" - a simple wildcard match, '*' = any character
+    ///     "T:xxx" - straight text match
+    ///     "W:xxx" - a normal wildcard match, '*' = any character, '?' = single character
+    /// </summary>
+    /// <param name="key">The key string.</param>
+    /// <returns>The search key.</returns>
     public static SearchKey Create(string key)
     {
-        if (key == null)
-            throw new SearchKeyException("Search key must not be null");
-
         char specifier;
         string actualKey;
         if (key.Length > 2 && key[1] == ':')
@@ -59,7 +85,11 @@ public class SearchKey
 
     private static string ConvertStar(string text) => text.Replace("\\*", ".*");
 
-    public bool Equals(string other) => Regex?.IsMatch(other) ?? _key == other;
-
+    /// <summary>
+    ///     Allows search keys to automatically be created from strings.
+    ///     See SearchKey.Create for further information.
+    /// </summary>
+    /// <param name="key">The string to convert.</param>
+    /// <returns>A search key.</returns>
     public static implicit operator SearchKey(string key) => Create(key);
 }
--- AssertThatTests/SearchKeyTests.cs
+++ AssertThatTests/SearchKeyTests.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Text.RegularExpressions;
 using AssertThatLibrary;
 using JetBrains.Annotations;
 
@@ -13,6 +14,22 @@ public class SearchKeyTests
     {
         var searchKey = SearchKey.Create(key);
         AssertThat.TestValue(searchKey.Equals("Value")).Is(matches[0]);
+    }
+
+    [Theory]
+    [ClassData(typeof(TestData))]
+    public void CheckTextOnlyFromConstructor(string key, bool[] matches)
+    {
+        var searchKey = new SearchKey(key);
+        AssertThat.TestValue(searchKey.Equals("Value")).Is(matches[4]);
+    }
+
+    [Theory]
+    [ClassData(typeof(TestData))]
+    public void CheckRegexpFromConstructor(string key, bool[] matches)
+    {
+        var searchKey = new SearchKey(new Regex(key, RegexOptions.Compiled));
+        AssertThat.TestValue(searchKey.Equals("Value12")).Is(matches[5]);
     }
 
     [Theory]
@@ -51,23 +68,24 @@ public class SearchKeyTests
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-            yield return ["V", new[] { false, true, false, false }];
-            yield return ["Value", new[] { true, false, false, false }];
-            yield return ["Other", new[] { false, false, false, false }];
-            yield return ["D:Value", new[] { true, false, false, false }];
-            yield return ["D:Value#", new[] { false, false, true, true }];
-            yield return ["D:*", new[] { true, true, true, true }];
-            yield return ["R:^Value[0-9]$", new[] { false, false, true, false }];
-            yield return ["R:^Value[0-9]+$", new[] { false, false, true, true }];
-            yield return ["R:^Value$", new[] { true, false, false, false }];
-            yield return ["R:Value", new[] { true, false, true, true }];
-            yield return ["R:.*", new[] { true, true, true, true }];
-            yield return ["S:Value", new[] { true, false, false, false }];
-            yield return ["S:*", new[] { true, true, true, true }];
-            yield return ["T:Value", new[] { true, false, false, false }];
-            yield return ["W:Value", new[] { true, false, false, false }];
-            yield return ["W:*", new[] { true, true, true, true }];
-            yield return ["W:?", new[] { false, true, false, false }];
+            yield return ["V", new[] { false, true, false, false, false, true }];
+            yield return ["Value", new[] { true, false, false, false, true, true }];
+            yield return ["Other", new[] { false, false, false, false, false, false }];
+            yield return ["D:Value", new[] { true, false, false, false, false, false }];
+            yield return ["D:Value#", new[] { false, false, true, true, false, false }];
+            yield return ["D:*", new[] { true, true, true, true, false, false }];
+            yield return ["R:^Value[0-9]$", new[] { false, false, true, false, false, false }];
+            yield return ["R:^Value[0-9]+$", new[] { false, false, true, true, false, false }];
+            yield return ["^Value[0-9]+$", new[] { false, false, false, false, false, true }];
+            yield return ["R:^Value$", new[] { true, false, false, false, false, false }];
+            yield return ["R:Value", new[] { true, false, true, true, false, false }];
+            yield return ["R:.*", new[] { true, true, true, true, false, false }];
+            yield return ["S:Value", new[] { true, false, false, false, false, false }];
+            yield return ["S:*", new[] { true, true, true, true, false, false }];
+            yield return ["T:Value", new[] { true, false, false, false, false, false }];
+            yield return ["W:Value", new[] { true, false, false, false, false, false }];
+            yield return ["W:*", new[] { true, true, true, true, false, false }];
+            yield return ["W:?", new[] { false, true, false, false, false, false }];
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
